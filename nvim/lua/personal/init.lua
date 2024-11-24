bb4860faec373dfb26c73520e8d1d8ff8b5a7173
Comte Zero <countzr@gmail.com>
2024-11-24 05:56:55 +0100

    nvim: detect if in WSL

--- nvim/lua/personal/init.lua
+++ nvim/lua/personal/init.lua
@@ -2,16 +2,19 @@ require("personal.packer")
 require("personal.remap")
 
 vim.cmd('set clipboard+=unnamedplus')
-
-vim.g.clipboard = {
-	name = 'WslClipboard',
-	copy = {
-		["+"] = 'clip.exe',
-		["*"] = 'clip.exe',
-	},
-	paste = {
-		["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-		["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-	},
-	cache_enabled = 0,
-}
+-- if linux and wsl
+local in_wsl = os.getenv('WSL_DISTRO_NAME')
+if in_wsl then
+	vim.g.clipboard = {
+		name = 'WslClipboard',
+		copy = {
+			["+"] = 'clip.exe',
+			["*"] = 'clip.exe',
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
+end
